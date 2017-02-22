42be08e4d2967d7aa1c04cc8d9856843c8a4b56b
socialmedia7 <charly.lara007@gmail.com>
2017-02-22 15:32:29 -0800

    Carlos: Se modifico el archivo
    Se dividira en dos parte el archivo , para el ususario y para las mensiones

--- usertimeline.R
+++ usertimeline.R
@@ -1,25 +1,23 @@
 ###Entra a Twiterr
-
-###Entra a Twiterr
 library(RCurl)
 library(tm)
 library(twitteR)
 library(ROAuth)
+library(RColorBrewer)
+
 api_key <- "TEqtGTHhGpl0gaEj0YlacXSMP"
 api_secret <- "C2Uyt9dN8REdTlTkRBY7gFtMhLgVeNtbpEjefKpiBKO8Ux7ns0"
 access_token <- "831261005850120192-LQ39DWYq0KZOXqwldxMWHwB1AYUgdUv"
 access_token_secret <- "z5PBlltSCwh5wuiXfgE0BHI5aR19QUmvPg0d5TzNeblBF"
 setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
 
-##### Limpieza de Texto
-
-#######
+##### LIMPIEZA DE TEXTO PARA USUARIO##############################################
 tweets <- userTimeline("marianaSalas", n = 3200) #Cargar en la variable tweets la informaciÃ³n de todos los twetter que  ella puso
-retweets<-searchTwitter('@marianaSalas',n=1000) #Cargar en la variable retweets la informaciÃ³n de todos los twetter en los que la mencionaron
+retweets<-searchTwitter('@marianaSalas',n=3200) #Cargar en la variable retweets la informaciÃ³n de todos los twetter en los que la mencionaron
 tweets.df <- twListToDF(tweets)#hacer un data frame con la informaciÃ³n en tweets
 retweets.df<-twListToDF(retweets)#hacer un data frame con la informaciÃ³n en retweets
 #dump('tweets.df',file=xxx) Crea un archivo .txt en la ruta que ingreses en file
-
+############# Tweets personales
 myCorpus <- Corpus(VectorSource(tweets.df$text))
 #VectorSource lo que hace es poner en un  vector el data frame, es decir, cada tweets es una entrada del vector.
 #La function Corpus convierte cada entrada del vector en una lista.
@@ -27,55 +25,97 @@ myCorpus <- Corpus(VectorSource(tweets.df$text))
 myCorpus <- tm_map(myCorpus, content_transformer(tolower))#Lo que hace tm_map es que al archivo le hace tal cambio
 #el cambio content_transformer(tolower) es que lo convierte en un archivo de texto plano
 
-###### Remueve el URL de http
-removeURL <- function(x) gsub("http[^[:space:]]*", "", x)
-####### convierte todas las palabras a minusculas
-myCorpus <- tm_map(myCorpus, content_transformer(removeURL))
+removeURL <- function(x) gsub("http[^[:space:]]*", "", x) # Remueve el Url de los tweets 
+myCorpus <- tm_map(myCorpus, content_transformer(removeURL))# Remueve el Url de los tweets 
 
+# eliminar aparte de las letras inglesas o nada de espacio
 removeNumPunct <- function(x) gsub("[^[:alpha:][:space:]]*", "", x)
-
 myCorpus <- tm_map(myCorpus, content_transformer(removeNumPunct))
 
 # Remueve palabras como pronombres y articulos.
-
 myStopwords <- c(stopwords("spanish"), "un","la","el")
-
 myCorpus <- tm_map(myCorpus, removeWords, myStopwords)
+
 # remove extra el espacio extra
 myCorpus <- tm_map(myCorpus, stripWhitespace)
-
-
 myCorpusCopy <- myCorpus
-
 myCorpus <- tm_map(myCorpus, stemDocument)
 
-#########
 
+########### LIMPIEZA DE TEXTO PARA MENSIONES##########################################
+myCorpusr <- Corpus(VectorSource(retweets.df$text))
+#VectorSource lo que hace es poner en un  vector el data frame, es decir, cada tweets es una entrada del vector.
+#La function Corpus convierte cada entrada del vector en una lista.
+
+myCorpusr <- tm_map(myCorpusr, content_transformer(tolower))#Lo que hace tm_map es que al archivo le hace tal cambio
+#el cambio content_transformer(tolower) es que lo convierte en un archivo de texto plano
+
+removeURLr <- function(x) gsub("http[^[:space:]]*", "", x) # Remueve el Url de los tweets 
+myCorpus <- tm_map(myCorpusr, content_transformer(removeURLr))# Remueve el Url de los tweets 
+
+# eliminar aparte de las letras inglesas o nada de espacio
+removeNumPunctr <- function(x) gsub("[^[:alpha:][:space:]]*", "", x)
+myCorpusr <- tm_map(myCorpusr, content_transformer(removeNumPunctr))
+
+# Remueve palabras como pronombres y articulos.
+myStopwordsr <- c(stopwords("spanish"), "un","la","el" ,"epn", "rt" )
+myCorpusr <- tm_map(myCorpusr, removeWords, myStopwordsr)
+
+# remove extra el espacio extra
+myCorpusr <- tm_map(myCorpusr, stripWhitespace)
+myCorpusCopyr <- myCorpusr
+myCorpusr <- tm_map(myCorpusr, stemDocument)
+
+###############################################################
+
+
+#######################################################
+######################################################
+##     MATRIZ DOCUMENTO TERMINO
+#Una matriz de documento -término o matriz de término- 
+#documento es una matriz matemática que describe la frecuencia
+#de los términos que se producen en una colección de documentos. 
+#En una matriz de documento-término, las filas corresponden a documentos 
+#en la colección y las columnas corresponden a términos. 
+#Hay varios esquemas para determinar el valor que cada entrada en la matriz debe tomar. 
+#Uno de estos esquemas es tf-idf . Son útiles en el campo del procesamiento del lenguaje natural
 tdm <- TermDocumentMatrix(myCorpus, control = list(wordLengths = c(1, Inf)))
+tdmr<-TermDocumentMatrix(myCorpusr, control = list(wordLengths = c(1, Inf)))
+###Ejecucion de Analisis###################
 tdm
+tdmr
 
-(freq.terms <- findFreqTerms(tdm, lowfreq=3))
+######## Ejecucion de Palabras Mayores a 40
+(freq.terms <- findFreqTerms(tdm, lowfreq=40)
+(freq.termsr <- findFreqTerms(tdmr, lowfreq=40))
 
+#########Grafico de Frecuencias (ggplot 2) para Tweets###############
+
+library(ggplot2)
 term.freq <- rowSums(as.matrix(tdm))
-term.freq <- subset(term.freq, term.freq >=7)
-df <- data.frame(term = names(term.freq), freq = term.freq)
-
-
-
-#########Grafico de Frecuencias###############
-library(ggplot2)   
+term.freq <- subset(term.freq, term.freq >=40) ## Extraer los terminos con mayores a 45 repeticiones
+df <- data.frame(term = names(term.freq), freq = term.freq) ### Crear el data.frame con nombre 
+#de las palabras en  y frecuencias en y
 
 p<- ggplot(df, aes(x=term, y=freq)) + geom_bar(stat = "identity") + xlab("Palabra") + ylab("Frecuencia") +coord_flip()
 p <- p + theme(axis.text.x=element_text(angle=45, hjust=1))   
-p
-############################################
+p ##Se ejecuta la grafica
 
 
-########### Nube de Palabras##############################
+#########Grafico de Frecuencias (ggplot 2) para menciones###############
+library(ggplot2)
+term.freqr <- rowSums(as.matrix(tdmr))
+term.freqr <- subset(term.freqr, term.freqr >=40) ## Extraer los terminos con mayores a 45 repeticiones
+dfr <- data.frame(term = names(term.freqr), freq = term.freqr) ### Crear el data.frame con nombre 
+#de las palabras en  y frecuencias en y
+
+pr<- ggplot(dfr, aes(x=term, y=freq)) + geom_bar(stat = "identity") + xlab("Palabra") + ylab("Frecuencia") +coord_flip()
+p <- pr + theme(axis.text.x=element_text(angle=45, hjust=1))   
+pr ##Se ejecuta la grafica
+
+################## Nube de Palabras de los Tweets##############################
 library(wordcloud)
-set.seed(1234)
-
-
+set.seed(1234) #### Numero aleatorio
 m <- as.matrix(tdm)
 ##### Calcula la nube de palabras con un maximo de 200
 word.freq <- sort(rowSums(m), decreasing = T)
@@ -84,14 +124,16 @@ wordcloud(words = names(word.freq), freq = word.freq, min.freq = 1, max.words=20
 
 #########################################################
 
+################## Nube de Palabras de las mensiones##############################
+library(wordcloud)
+set.seed(1234) #### Numero aleatorio
+mr <- as.matrix(tdmr)
+##### Calcula la nube de palabras con un maximo de 200
+word.freqr <- sort(rowSums(mr), decreasing = T)
+wordcloud(words = names(word.freqr), freq = word.freqr, min.freq = 1, max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
 
-
-
-
-
-
-
-
+#########################################################
 ############################################################################
 # remove sparse terms
 tdm2 <- removeSparseTerms(tdm, sparse = 0.95)
@@ -157,8 +199,8 @@ for (i in 1:k){
 
 dtm <- as.DocumentTermMatrix(tdm)
 library(topicmodels)
-lda <- LDA(dtm, k = 5)
-term <- terms(lda, 5) # first 7 terms of every topic
+lda <- LDA(dtm, k = 6)
+term <- terms(lda, 6) 
 (term <- apply(term, MARGIN = 2, paste, collapse = ", "))
 library("lda")
 
@@ -171,10 +213,6 @@ topics <- data.frame(date=as.IDate(tweets.df$created), topic)
 qplot(date, ..count.., data=topics, geom="density",
       fill=term[topic], position="stack")
 #############################################################
-
-
-
-
 
 ######### Analisis de Sentimientos
 require(devtools)
@@ -193,7 +231,9 @@ result <- aggregate(score ~ date, data = sentiments, sum)
 plot(result, type = "l")
 ############################
 
-#### Analisis de Grafos#####
+#### Analisis de Grafos#######
+
+library("igraph")
 m2[m2>=1] <- 1
 m4<- m2 %*% t(m2)
 
@@ -211,5 +251,8 @@ egam <- (log(E(g)$weight)+.4) / max(log(E(g)$weight)+.4)
 E(g)$width <- egam
 plot(g, layout=layout1)
 #############################
+
+
+
 
 
